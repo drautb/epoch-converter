0c677f5ae21e27e7b8c97c9ca68da3affa4de3c8
Ben Draut <drautb@gmail.com>
2013-01-26 09:21:34 -0700

    First Version Done - Beta
    Popup style, date format, etc, are all static. They cannot be changed.
    Position is always below click. Nothing can be customized, not
    extensively tested.

---
+++ selection.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $('body').append("<div id=\"ec-bubble\"><div id=\"ec-bubble-text\"></div><div id=\"ec-bubble-close\"></div></div>");
+    
+    /*var closeIconUrl = chrome.extension.getURL("close.png");
+    alert(closeIconUrl);
+    $('#ec-bubble-close').css('background-image', "url('" + closeIconUrl + "');");*/
+    $('#ec-bubble-close').click(function() {
+        hideBubble();
+    });
+    
+	$(document).dblclick(function(e) {
+		processSelection(e);
+	});
+	
+	$(document).bind('mouseup', function(e) {
+		processSelection(e);
+	});
+
+});
+
+function processSelection(e) {	
+    var text = getSelectedText();
+
+    if ($.isNumeric(text)) {
+		var humanReadableDate = convertTimestamp(text);
+        showBubble(e, humanReadableDate);        
+	}
+}
+
+function getSelectedText() {
+	var text = "";
+	
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+	
+	return text;
+}
+
+function convertTimestamp(ts) {
+	var date = new Date(ts * 1000);
+	var dateStr = "";
+    
+    var d = date.getDate();
+    var m = date.getMonth()+1;
+    var y = date.getFullYear();
+    dateStr += (m<=9?'0'+m:m) + "/" + (d<=9?'0'+d:d) + "/" + y + " - ";
+  
+    var h = date.getHours();
+    var mi = date.getMinutes();
+    var s = date.getSeconds(); 
+    dateStr += (h<=9?'0'+h:h) + ":" + (mi<=9?'0'+mi:mi) + ":" + (s<=9?'0'+s:s);
+    
+	return dateStr;
+}
+
+function showBubble(e, text) {
+    $('#ec-bubble').css('top', e.clientY + 20 + "px");
+    $('#ec-bubble').css('left', e.clientX - 85 + "px");
+    $('#ec-bubble-text').html(text);
+    $('#ec-bubble').css('visibility', 'visible');
+}
+
+function hideBubble() {
+    $('#ec-bubble').css('visibility', 'hidden');
+    $('#ec-bubble-text').html("");
+}
+
+
